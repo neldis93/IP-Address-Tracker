b19c213a943a19c8b371f90f7b849d2b77280110
Neldis Barrios <neldis12@hotmail.com>
2021-11-04 11:07:36 +0100

    Code settings

--- applications/geolocationip/views.py
+++ applications/geolocationip/views.py
@@ -5,15 +5,14 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, renderer_classes 
 from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
-
 from django.urls import reverse
 #from django.shortcuts import render
 #from django.views.generic import View
 from django.http import HttpResponseRedirect
 from django.contrib import messages
-
 from Geolocation.settings.base import get_secret
 
+## With Django rest framework Requests and Responses
 @api_view(['GET'])
 @renderer_classes([TemplateHTMLRenderer,JSONRenderer]) 
 def get_ip_tracker(request):
@@ -24,12 +23,13 @@ def get_ip_tracker(request):
     data = json.loads(api_url.content)
     data['api_key'] = api_key
     status = data.get('status')
+    
     if status == "fail":
         messages.error(request,'You have entered an invalid IP address!')
         return HttpResponseRedirect(reverse('ip_app:ip_tracker'))
     return Response(data,template_name='geolocationip/home.html')
 
-
+## With Django class-based views
 # class GetIpTracker(View):
 #     def get(self,request,*args,**kwargs):
 #         ip_address = request.GET.get('IP','')
